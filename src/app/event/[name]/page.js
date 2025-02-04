07b9f4e4ff712f260e7a11d0a6bb5c3554dc5412
Naman rai <namanrai309@gmail.com>
2025-02-04 13:51:44 +0530

    email change

--- src/app/event/[name]/page.js
+++ src/app/event/[name]/page.js
@@ -81,7 +81,7 @@ function page({ params }) {
     megha: {
       FormType: "general",
       emails: [
-        "20bei033@ietdavv.edu.in",
+        "testokie1@gmail.com",
         "megha.salian@influenceexchangegroup.com",
         "mohamed.suhel@influenceexchangegroup.com",
       ],
@@ -89,7 +89,7 @@ function page({ params }) {
     navya: {
       FormType: "general",
       emails: [
-        "20bei033@ietdavv.edu.in",
+        "testokie1@gmail.com",
         "megha.salian@influenceexchangegroup.com",
         "navya.kotian@influenceexchangegroup.com",
         "mohamed.suhel@influenceexchangegroup.com",
@@ -98,7 +98,7 @@ function page({ params }) {
     veera: {
       FormType: "general",
       emails: [
-        "20bei033@ietdavv.edu.in",
+        "testokie1@gmail.com",
         "megha.salian@influenceexchangegroup.com",
         "mohamed.suhel@influenceexchangegroup.com",
         "veera.crasta@influenceexchangegroup.com",
@@ -107,7 +107,7 @@ function page({ params }) {
     fathima: {
       FormType: "general",
       emails: [
-        "20bei033@ietdavv.edu.in",
+        "testokie1@gmail.com",
         "megha.salian@influenceexchangegroup.com",
         "mohamed.suhel@influenceexchangegroup.com",
         "fathima.shaikh@influenceexchangegroup.com",
@@ -116,7 +116,7 @@ function page({ params }) {
     mk: {
       FormType: "general",
       emails: [
-        "20bei033@ietdavv.edu.in",
+        "testokie1@gmail.com",
         "mohamed.suhel@influenceexchangegroup.com",
         "megha.kottary@influenceexchangegroup.com",
         "megha.salian@influenceexchangegroup.com",
@@ -125,7 +125,7 @@ function page({ params }) {
     priyanshi: {
       FormType: "speaker",
       emails: [
-        "20bei033@ietdavv.edu.in",
+        "testokie1@gmail.com",
         "mohamed.suhel@influenceexchangegroup.com",
         "priyanshi.pandey@influenceexchangegroup.com",
       ],
